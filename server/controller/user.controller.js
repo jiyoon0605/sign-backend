70ad17d2924d88bddd751d1c7cd2097cea6121c9
jiyoon0605 <ljy014@naver.com>
2021-04-02 10:57:46 +0900

    [FIX] login test

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -86,47 +86,47 @@ const sendEmail = async (req, res) => {
 const login = async (req, res) => {
   try {
     const user = await userSchema.find({ email: req.body.email });
-    if (user.length) {
-      const hashPassword = crypto
-        .createHash("sha512")
-        .update(req.body.password + user[0].salt)
-        .digest("hex")
-        .catch(() => res.status(403).json({ error: "비번 " }));
-      if (hashPassword !== user[0].password) {
-        return res.status(404).json({ error: "비밀번호가 틀렸습니다." });
-      }
-      // const token = jwt.sign(
-      //   {
-      //     id: user[0].id,
-      //     email: user[0].userEmail,
-      //     name: user[0].name,
-      //   },
-      //   process.env.SECRETKEY,
-      //   {
-      //     expiresIn: "12h",
-      //   }
-      // );
-      // const refreshToken = jwt.sign(
-      //   {
-      //     type: "refresh",
-      //   },
-      //   process.env.SECRETKEY,
-      //   {
-      //     expiresIn: "30d",
-      //   }
-      // );
-      //res.cookie("user", token);
-      return res.status(201).json({
-        result: "ok",
-        // token,
-        // refreshToken,
-      });
-    } else {
-      return res.status(404).json({ error: "이메일을 다시 확인해 주세요." });
-    }
+    // if (user.length) {
+    //   const hashPassword = crypto
+    //     .createHash("sha512")
+    //     .update(req.body.password + user[0].salt)
+    //     .digest("hex")
+    //     .catch(() => res.status(403).json({ error: "비번 " }));
+    //   if (hashPassword !== user[0].password) {
+    //     return res.status(404).json({ error: "비밀번호가 틀렸습니다." });
+    //   }
+    //   // const token = jwt.sign(
+    //   //   {
+    //   //     id: user[0].id,
+    //   //     email: user[0].userEmail,
+    //   //     name: user[0].name,
+    //   //   },
+    //   //   process.env.SECRETKEY,
+    //   //   {
+    //   //     expiresIn: "12h",
+    //   //   }
+    //   // );
+    //   // const refreshToken = jwt.sign(
+    //   //   {
+    //   //     type: "refresh",
+    //   //   },
+    //   //   process.env.SECRETKEY,
+    //   //   {
+    //   //     expiresIn: "30d",
+    //   //   }
+    //   // );
+    //   //res.cookie("user", token);
+    return res.status(201).json({
+      result: "ok",
+      user,
+      // token,
+      // refreshToken,
+    });
+    // } else {
+    //   return res.status(404).json({ error: "이메일을 다시 확인해 주세요." });
+    // }
   } catch (err) {
     console.log(err);
-    res.status(404).json({ error: err });
     res.status(404).json({ error: err });
   }
 };
