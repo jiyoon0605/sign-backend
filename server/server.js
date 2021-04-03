327a3dcbacdc39324d516f308f5023b46700d1ce
jiyoon0605 <ljy014@naver.com>
2021-04-03 19:04:35 +0900

    fix

--- server/server.js
+++ server/server.js
@@ -36,15 +36,6 @@ app.use(
   })
 );
 
-router.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, X-Auth-Token, Accept"
-  );
-});
-
 app.use(cors(corsOption));
 
 app.use(express.json());
